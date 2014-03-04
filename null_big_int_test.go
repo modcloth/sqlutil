fcc21bbf3deda5698307fb6fe9480dbe505328c3
Jesse Szwedko <j.szwedko@modcloth.com>
2014-03-04 14:36:04 -0500

    Adding tests for NullBigInt.Scan()

--- null_big_int_test.go
+++ null_big_int_test.go
@@ -2,8 +2,11 @@ package sqlutil_test
 
 import (
 	"database/sql/driver"
+	"errors"
 	"math/big"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/modcloth/sqlutil"
 )
@@ -28,6 +31,35 @@ func TestNullBigIntValue(t *testing.T) {
 
 		if actual != tt.expected {
 			t.Errorf("%+v.Value(): expected %s, actual %s", tt.n, tt.expected, actual)
+		}
+	}
+}
+
+func TestNullBigIntScan(t *testing.T) {
+	var tests = []struct {
+		n        driver.Value
+		expected sqlutil.NullBigInt
+		err      error
+	}{
+		{int64(2), sqlutil.NullBigInt{BigInt: sqlutil.BigInt{Int: *big.NewInt(2)}, Valid: true}, nil},
+		{float64(2.5), sqlutil.NullBigInt{}, errors.New("Couldn't scan float64")},
+		{true, sqlutil.NullBigInt{}, errors.New("Couldn't scan bool")},
+		{[]byte{5}, sqlutil.NullBigInt{}, errors.New("Couldn't scan []uint8")},
+		{"2", sqlutil.NullBigInt{BigInt: sqlutil.BigInt{Int: *big.NewInt(2)}, Valid: true}, nil},
+		{time.Now(), sqlutil.NullBigInt{}, errors.New("Couldn't scan time.Time")},
+		{nil, sqlutil.NullBigInt{}, nil},
+	}
+
+	for _, tt := range tests {
+		actual := sqlutil.NullBigInt{}
+		err := actual.Scan(tt.n)
+
+		if !reflect.DeepEqual(err, tt.err) {
+			t.Errorf("%+v.Scan(%v): expected error %+v, got error: %+v", actual, tt.n, tt.err, err)
+		}
+
+		if actual.Valid != tt.expected.Valid || actual.BigInt.Int.Cmp(&tt.expected.BigInt.Int) != 0 {
+			t.Errorf("%+v.Scan(%v): expected %+v, actual: %+v", actual, tt.n, tt.expected, actual)
 		}
 	}
 }
